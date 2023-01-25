26262ffd734c26ea81b8cb94d81fb299760c9ebe
Polalius <napakant1235@gmail.com>
2023-01-25 21:04:53 +0700

    entity exerciseprogram_system - close #32

---
+++ backend/entity/exerciseprogram_system.go
@@ -0,0 +1,40 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type WormUp struct {
+	gorm.Model
+	SetName          string
+	ExercisePoseture string
+
+	ExerciseProgramList []ExerciseProgramList `gorm:"foreignKey:WormUpID"`
+}
+type Exercise struct {
+	gorm.Model
+	SetName          string
+	ExercisePoseture string
+
+	ExerciseProgramList []ExerciseProgramList `gorm:"foreignKey:ExerciseID"`
+}
+type Stretch struct {
+	gorm.Model
+	SetName          string
+	ExercisePoseture string
+
+	ExerciseProgramList []ExerciseProgramList `gorm:"foreignKey:StretchID"`
+}
+type ExerciseProgramList struct {
+	gorm.Model
+	ProgramName string
+	EmployeeID  *uint
+	Employee    Employee
+	WormUpID    *uint
+	WormUp      WormUp
+	ExerciseID  *uint
+	Exercise    Exercise
+	StretchID   *uint
+	Stretch     Stretch
+	Minute      uint
+}
