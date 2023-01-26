f2cdb92c03872cfc7385faf0504ffadebe61926e
Polalius <napakant1235@gmail.com>
2023-01-27 05:02:46 +0700

    controller signin - close #69

---
+++ backend/controller/Signin/signin.go
@@ -0,0 +1,77 @@
+package signin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team18/entity"
+)
+
+//-----------Part Signin -----------
+
+// Get /users
+// List All User
+func ListUser(c *gin.Context) {
+	var users []entity.User
+	if err := entity.DB().Raw("SELECT * FROM users").Scan(&users).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+		"data":   users,
+	})
+}
+
+// Get /user/:id
+// Get user
+func GetUser(c *gin.Context) {
+	var user entity.User
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM users WHERE id = ?", id).Scan(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": "OK",
+		"data":   user,
+	})
+}
+
+// PATCH /users
+func UpdateUser(c *gin.Context) {
+	var user entity.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", user.ID).First(&user); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	if err := entity.DB().Save(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "Update Success",
+		"data":   user,
+	})
+}
+
+// DELETE /users/:id
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM users WHETE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
---
+++ backend/middlewares/authorize.go
@@ -0,0 +1,62 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team18/services"
+)
+
+func Authorizes() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("Authorization")
+		if clientToken == "" {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "No Authorization header provided"})
+			return
+		}
+
+		extractedToken := strings.Split(clientToken, "Bearer ")
+
+		if len(extractedToken) == 2 {
+			clientToken = strings.TrimSpace(extractedToken[1])
+		} else {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Incorrect Format of Authorization Token"})
+			return
+		}
+
+		jwtWrapper := services.JwtWrapper{
+			SecretKey: "SvNQpBN8y3qlVrsGAYYWoJJk56LtzFHx",
+			Issuer:    "AuthService",
+		}
+
+		claims, err := jwtWrapper.ValidateToken(clientToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.Set("user_id", claims.User_id)
+		c.Set("role_name", claims.Role_name)
+
+		c.Next()
+	}
+
+}
+
+func CheckAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role := c.GetString("role_name")
+
+		if role != "admin" {
+			c.JSON(http.StatusBadGateway, gin.H{
+				"error": "you is not admin",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
