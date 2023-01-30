dc0fdeb3067e629fad10cb7110be33649af86a96
Polalius <111460651+Polalius@users.noreply.github.com>
2023-01-30 19:44:06 +0700

    entity trainerbooking - close #41

---
+++ backend/entity/trainerbooking.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type TrainerBookingList struct {
+	gorm.Model
+	EmployeeID *uint
+	Employee Employee
+
+	MemberID *uint
+	Member Member
+
+	ExerciseProgramListID *uint
+	ExerciseProgramList ExerciseProgramList
+
+	Training_Time time.Time
+
+}
